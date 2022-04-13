db56340b1d7ad7a17b01ea3c9bf94eb5046f77ea
Tim Bryan <tim.bryan@drpgroup.com>
2022-04-13 13:16:07 +0100

    replace individual statements with GSAP timeline

--- src/js/gsap3.jsx
+++ src/js/gsap3.jsx
@@ -1,81 +1,110 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { gsap } from 'gsap'
 import imgSwirl from '../images/swirl.png'
 
-const floatDownAnim = {
-  opacity: 0,
-  duration: 1,
-  y: -50,
-  stagger: 0.6
-}
-
-const floatUpAnim = {
-  duration: 1,
-  y: 30,
-  opacity: 0,
-  delay: 1.4
-}
-
-const swipeUpAnim = {
-  scaleY: 0,
-  duration: 1
-}
-
-const slideInFromRightAnim = {
-  duration: 1,
-  delay: 1.1,
-  backgroundPosition: '200px 0',
-  opacity: 0
+const ANIMATIONS = {
+  floatDown: {
+    opacity: 0,
+    stagger: 0.6,
+    y: -50
+  },
+  floatUp: {
+    opacity: 0,
+    y: 50
+  },
+  swipeUp: {
+    scaleY: 0
+  },
+  slideInFromRight: {
+    backgroundPosition: '200px 0',
+    opacity: 0
+  }
 }
 
 const GSAP3 = () => {
   const asideRef = useRef(null)
   const contentRef = useRef(null)
   const imageRef = useRef(null)
-
+  const gsapTimelineRef = useRef(null)
+  const [reversed, setReversed] = useState(null)
   const gsapSelector = gsap.utils.selector(contentRef)
 
+  // Use gsap directly for one-off animations
+  function onEnter ({ currentTarget }) {
+    gsap.to(currentTarget, { rotate: '1deg', scale: 1.1 })
+  }
+
+  function onLeave ({ currentTarget }) {
+    gsap.to(currentTarget, { rotate: '0deg', scale: 1 })
+  }
+
+  // Using the timeline for repeated animations
+  // Inside of a useEffect hook to ensure the timeline is only created once
   useEffect(() => {
-    gsap.from(gsapSelector('.floatDownAnim'), floatDownAnim)
+    gsapTimelineRef.current = gsap.timeline({
+      defaults: { duration: 1 }
+    })
 
-    gsap.from(imageRef.current, floatUpAnim)
+    gsapTimelineRef.current
+      .from(gsapSelector('.floatDown'), ANIMATIONS.floatDown)
+      .to(
+        gsapSelector('.swipeUp'),
+        { ...ANIMATIONS.swipeUp, duration: 1.8 },
+        '-=2.2'
+      )
+      .from(asideRef.current, ANIMATIONS.slideInFromRight, '-=1.5')
+      .from(imageRef.current, ANIMATIONS.floatUp, '-=1.5')
+  }, [])
 
-    gsap.to(gsapSelector('.swipeUpAnim'), swipeUpAnim)
+  useEffect(() => {
+    // skip logic on initial render
+    if (reversed === null) return
 
-    gsap.from(asideRef.current, slideInFromRightAnim)
-  })
+    if (reversed) {
+      gsapTimelineRef.current.play()
+      return
+    }
+
+    gsapTimelineRef.current.reverse()
+    setTimeout(() => gsapTimelineRef.current.play(), 1800)
+  }, [reversed])
 
   return (
     <div id='gsap'>
       <main>
         <div className='content' ref={contentRef}>
-          <h1 className='floatDownAnim'>
+          <h1 className='floatDown'>
             <div>
-              <span className='swipeUpAnim' />
+              <span className='swipeUp' />
               Bridging the gap
             </div>{' '}
             <div>
-              <span className='swipeUpAnim' />
+              <span className='swipeUp' />
               between dream
             </div>{' '}
             <div>
-              <span className='swipeUpAnim' />
+              <span className='swipeUp' />
               and reality
             </div>
           </h1>
-          <p className='floatDownAnim'>
+          <p className='floatDown'>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio at
             ipsa alias modi natus excepturi?
           </p>
-          <a href='#' className='floatDownAnim' id='cta'>
+          <button
+            className='floatDown'
+            onClick={() => setReversed(prev => !prev)}
+            onMouseEnter={onEnter}
+            onMouseLeave={onLeave}
+          >
             Start Dreaming
-          </a>
+          </button>
         </div>
       </main>
-      <aside ref={asideRef} className='slideInFromRightAnim'>
+      <aside ref={asideRef} className='slideInFromRight'>
         <div className='wrapper'>
           <img
-            className='swirl floatUpAnim'
+            className='swirl floatUp'
             src={imgSwirl}
             alt='Swirl Graphic'
             ref={imageRef}
